08f553f7d5a4584674c70fce6c5802f9b5abd014
kroman02 <kory.graphic@gmail.com>
2022-09-17 11:24:53 +0100

    changed inner gallery elements to divs from img due to Chrome display issue

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -4,8 +4,9 @@ import '../styles/design.css'
 const Design = () => {
 
     return (
+        <>
+        <div className="left_deco"></div>
     <div className="gallery">
-        
         <img src='../../public/savvatore.gif' />
         <img src='../../public/a1.jpg' />
         <img className="lion" src='../../public/lion2.png' />
@@ -14,12 +15,15 @@ const Design = () => {
         <div className="design_header"><h1>Design</h1></div>
         <img src='../../public/Df.jpg' />
         <div className="inner_gallery">
-                <img src='../../public/EMPIRE1.jpg' />
-                <img src='../../public/life.png' />
+                {/* <img src='../../public/EMPIRE1.jpg' />
+                <img src='../../public/life.png' /> */}
+                <div className="inner_images empire"></div>
+                <div className="inner_images life"></div>
         </div>
         <img className="waterplane" src='../../public/waterplane.png' />
-
     </div>
+    <div className="right_deco"></div>
+    </>
 )
 
 }
